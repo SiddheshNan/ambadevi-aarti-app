3465e00921c2d24cf2ec169ee9d93afd27bdc8e8
Siddhesh <siddhesh.nandurkar@gmail.com>
2023-02-24 08:34:29 +0530

    minor aarti changes + docs added

--- mappings/other-pdf/file-imports.js
+++ mappings/other-pdf/file-imports.js
@@ -4,10 +4,10 @@ export const otherPDF1 = Asset.fromModule(
   require("../../docs/other-pdf/dainik_prarthana.pdf")
 );
 export const otherPDF2 = Asset.fromModule(
-  require("../../docs/other-pdf/mahadevachi_aarti_shivshankar_girijadhar.pdf")
+  require("../../docs/other-pdf/janardan_swami_1.pdf")
 );
 export const otherPDF3 = Asset.fromModule(
-  require("../../docs/other-pdf/janardan_swami_aarti_2.pdf")
+  require("../../docs/other-pdf/janardan_swami_2.pdf")
 );
 export const otherPDF4 = Asset.fromModule(
   require("../../docs/other-pdf/shri_vyankatesh_aarti.pdf")
@@ -19,17 +19,20 @@ export const otherPDF6 = Asset.fromModule(
   require("../../docs/other-pdf/gajanan_maharaj_aarti.pdf")
 );
 export const otherPDF7 = Asset.fromModule(
-  require("../../docs/other-pdf/shankarachi_aarti_jay_jay_tryambakraj.pdf")
+  require("../../docs/other-pdf/shankar_aarti_shivshankar_girijadhar.pdf")
 );
 export const otherPDF8 = Asset.fromModule(
   require("../../docs/other-pdf/shankarachi_aarti_lavthavti_vikrala.pdf")
 );
 export const otherPDF9 = Asset.fromModule(
-  require("../../docs/other-pdf/aarti_sangrah.pdf")
+  require("../../docs/other-pdf/shankarachi_aarti_jay_jay_tryambakraj.pdf")
 );
 export const otherPDF10 = Asset.fromModule(
-  require("../../docs/other-pdf/ashtak_pustika_1.pdf")
+  require("../../docs/other-pdf/aarti_sangrah.pdf")
 );
 export const otherPDF11 = Asset.fromModule(
+  require("../../docs/other-pdf/ashtak_pustika_1.pdf")
+);
+export const otherPDF12 = Asset.fromModule(
   require("../../docs/other-pdf/ashtak_pustika_2.pdf")
 );
